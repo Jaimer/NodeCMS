fc1c370ceae16a036697782ce8e998189fff68c6
Jaime Moscoso <jaime.r.moscoso@gmail.com>
2018-10-27 01:50:47 -0500

    Admin View

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', exphbs({defaultLayout: 'home'}));
 app.set('view engine', 'handlebars');
 
-const main = require('./routes/home/main');
+const main = require('./routes/home/index');
+const admin = require('./routes/admin/index');
 
 app.use('/', main);
+app.use('/admin', admin);
 
 app.listen(4500, ()=>{
     console.log(`listening on port 4500`);
